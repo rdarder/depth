034effe2ff2c4c59f608e20639f0fa84b089ad16
Rafael Darder <darder@gmail.com>
2025-06-26 12:25:19 -0300

    Change loss level weighting so that gradients don't flow through the weights calculations.

--- src/depth/model/loss.py
+++ src/depth/model/loss.py
@@ -9,10 +9,18 @@ def frame_pair_loss(model: FramePairFlow, f1: jax.Array, f2: jax.Array, priors: 
     flow_pyramid_with_loss, img_pyramid1, img_pyramid2 = model(f1, f2, priors)
     level_losses = jnp.array([jnp.mean(level[:, :, :, 2]) for level in flow_pyramid_with_loss])
     ratio_numerators = level_losses[:-1]
-    ratio_denominators = level_losses[1:]
+    ratio_denominators = level_losses[1:] + 1e-6
     level_losses_ratios = ratio_numerators / ratio_denominators
-    weights = softmax(level_losses_ratios)
-    weighted_loss = jnp.mean(ratio_numerators * weights)
-    aux = dict(pyramid1=img_pyramid1, pyramid2=img_pyramid2, flow_with_loss=flow_pyramid_with_loss,
-               levels_weights=weights, levels_losses=level_losses)
+    level_losses_ratios_stopped = jax.lax.stop_gradient(level_losses_ratios)
+    level_losses_ratios_extended = jnp.concatenate(
+        [level_losses_ratios_stopped, level_losses_ratios_stopped[-1:] / 2]
+    )
+    weights = level_losses_ratios_extended / jnp.sum(level_losses_ratios_extended)
+    weighted_loss = jnp.sum(level_losses * weights)
+    aux = dict(pyramid1=img_pyramid1,
+               pyramid2=img_pyramid2,
+               flow_with_loss=flow_pyramid_with_loss,
+               levels_weights=weights,
+               levels_losses=level_losses)
+
     return weighted_loss, aux
