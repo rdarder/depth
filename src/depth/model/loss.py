925c59cd117b141f95984809972175af167a562c
Rafael Darder <darder@gmail.com>
2025-06-27 11:58:04 -0300

    simplified training loop parameter handling. restore two levels on the first training level and restore last two level weights on the loss function.

--- src/depth/model/loss.py
+++ src/depth/model/loss.py
@@ -2,6 +2,7 @@ from typing import Sequence, Any
 
 import jax
 import jax.numpy as jnp
+from flax import nnx
 
 from depth.model.multi_level_flow import PyramidFlowEstimator
 
@@ -12,11 +13,16 @@ def frame_pair_loss(model: PyramidFlowEstimator,
                     priors: jax.Array) -> tuple[jax.Array, dict[str, Any]]:
     flow_pyramid_with_loss = model(pyramid1, pyramid2, priors)
     level_losses = jnp.array([jnp.mean(level[:, :, :, 2]) for level in flow_pyramid_with_loss])
-    weighted_loss = level_losses[0]
+    weights = jnp.array([1., 0.5])
+    weights = weights / jnp.sum(weights)
+    weighted_loss = jnp.sum(level_losses[:2] * weights)
     aux = dict(pyramid1=pyramid1,
                pyramid2=pyramid2,
                flow_with_loss=flow_pyramid_with_loss,
-               levels_weights=jnp.array([1.0]),
+               levels_weights=weights,
                levels_losses=level_losses)
 
     return weighted_loss, aux
+
+
+frame_pair_loss_value_with_grad = nnx.value_and_grad(frame_pair_loss, has_aux=True)
--- src/depth/training/train.py
+++ src/depth/training/train.py
@@ -1,9 +1,7 @@
 import os
 from datetime import datetime
-from pathlib import Path
 from typing import Sequence
 
-import flax.nnx
 import flax.nnx as nnx
 import jax
 import jax.numpy as jnp
@@ -13,93 +11,81 @@ from tensorboardX import SummaryWriter
 
 from depth.model.build import make_model
 from depth.train.build import generate_zero_priors
-from depth.model.loss import frame_pair_loss
+from depth.model.loss import frame_pair_loss_value_with_grad
 from depth.model.multi_level_flow import PyramidFlowEstimator
 from depth.model.settings import Settings
 from depth.training.build import make_frame_pyramids_dataset
 from depth.training.log import log_train_progress
 
-frame_pair_loss_with_grad = nnx.value_and_grad(frame_pair_loss, has_aux=True)
-
 
 @nnx.jit
 def train_step(model: PyramidFlowEstimator, optimizer: nnx.Optimizer,
                p1: Sequence[jax.Array], p2: Sequence[jax.Array], priors: jax.Array):
-    (loss, aux), grads = frame_pair_loss_with_grad(model, p1, p2, priors)
+    (loss, aux), grads = frame_pair_loss_value_with_grad(model, p1, p2, priors)
     optimizer.update(grads)
     return loss, aux
 
 
-def single_level_train_loop(model: PyramidFlowEstimator,
-                            optimizer: flax.nnx.Optimizer,
-                            settings: Settings,
-                            logger: SummaryWriter,
-                            checkpointer: StandardCheckpointer,
-                            stage: int,
-                            global_step: int,
-                            epochs: int
-                            ) -> nnx.Module:
-    keep_levels = stage + 1
-    train_dataset = make_frame_pyramids_dataset(settings, levels=keep_levels)
-    priors = generate_zero_priors(settings.train.batch_size, settings.model)
-    print(f"Starting stage {stage} training loop, using {keep_levels} pyramid levels.")
-    print(f"{len(train_dataset)} frame pairs on {settings.train.batch_size} size batches "
-          f"over {settings.train.num_epochs} epochs")
+class Train:
+    def __init__(self, settings: Settings):
+        self._settings = settings
+        self._run_id = datetime.now().strftime("%Y%m%d-%H%M%S")
+        self._checkpointer = StandardCheckpointer()
+        log_path = os.path.join(self._settings.train.tensorboard_logs, self._run_id)
+        self._logger = SummaryWriter(log_path)
+        print("Initializing model and optimizer...")
+        self._model = make_model(0, train=True, settings=self._settings.model)
+        self._optimizer = nnx.Optimizer(self._model, optax.chain(
+            optax.clip_by_global_norm(1.0),
+            optax.adam(learning_rate=self._settings.train.learning_rate)
+        ))
 
-    for epoch in range(epochs):
-        print(f"Epoch {epoch}")
-        for step, (f1_jax, f2_jax) in enumerate(train_dataset):
-            loss_value, aux = train_step(model, optimizer, f1_jax, f2_jax, priors)
-            if not jnp.isfinite(loss_value):
-                print(f"Warning: NaN or Inf loss detected at step {step}. Exiting training.")
-                break
-            if global_step % 100 == 0:
-                log_train_progress(aux, global_step, loss_value, logger)
-            global_step += 1
+    def run(self):
+        print("Starting main training loop...")
+        global_step = 0
+        epochs = self._settings.train.num_epochs
+        for stage in range(self._settings.model.levels - 1):
+            global_step = self.single_level_train_loop(stage, global_step, epochs)
+            epochs = 2 * epochs
+            self._save_checkpoint(f"{stage}-final")
+        print("Training finished.")
+        self._logger.close()
+        self._save_checkpoint(f"final")
 
-    return global_step
+    def single_level_train_loop(self, stage: int, global_step: int, epochs: int) -> int:
+        keep_levels = stage + 2
+        train_dataset = make_frame_pyramids_dataset(self._settings, levels=keep_levels)
+        priors = generate_zero_priors(self._settings.train.batch_size, self._settings.model)
+        print(f"Starting stage {stage} training loop, using {keep_levels} pyramid levels.")
+        print(f"{len(train_dataset)} frame pairs on {self._settings.train.batch_size} size batches "
+              f"over {self._settings.train.num_epochs} epochs")
 
+        for epoch in range(epochs):
+            print(f"Epoch {epoch}")
+            for step, (f1_jax, f2_jax) in enumerate(train_dataset):
+                loss_value, aux = train_step(self._model, self._optimizer, f1_jax, f2_jax, priors)
+                if not jnp.isfinite(loss_value):
+                    print(f"Warning: NaN or Inf loss detected at step {step}. Exiting training.")
+                    break
+                if global_step % 100 == 0:
+                    log_train_progress(aux, global_step, loss_value, self._logger)
+                global_step += 1
 
-def train_loop(settings: Settings):
-    current_time = datetime.now().strftime("%Y%m%d-%H%M%S")
-    log_path = os.path.join(settings.train.tensorboard_logs, current_time)
-    logger = SummaryWriter(log_path)
+        return global_step
 
-    os.makedirs(settings.train.checkpoint_dir, exist_ok=True)
-    checkpoint_dir = settings.train.checkpoint_dir.absolute() / current_time
-    checkpointer = StandardCheckpointer()
-    print("Starting main training loop...")
-    print("Initializing model and optimizer...")
-    model = make_model(0, train=True, settings=settings.model)
-    optimizer = nnx.Optimizer(model, optax.chain(
-        optax.clip_by_global_norm(1.0),
-        optax.adam(learning_rate=settings.train.learning_rate)
-    ))
-    global_step = 0
-    epochs = settings.train.num_epochs
-    for stage in range(settings.model.levels - 1):
-        global_step = single_level_train_loop(
-            model, optimizer, settings, logger, checkpointer, stage, global_step, epochs
-        )
-        epochs = 2 * epochs
-        save_checkpoint(model, checkpointer, checkpoint_dir, f"{stage}-final")
-    print("Training finished.")
-    logger.close()
-    save_checkpoint(model, checkpointer, checkpoint_dir, 'final')
-
-
-def save_checkpoint(model: PyramidFlowEstimator, checkpointer: StandardCheckpointer,
-                    checkpoint_dir: Path, tag: str):
-    model_state = nnx.state(model)
-    checkpointer.save(checkpoint_dir / tag, model_state)
-    checkpointer.wait_until_finished()
-    print(f"Saved checkpoint: {tag}.")
+    def _save_checkpoint(self, tag: str):
+        os.makedirs(self._settings.train.checkpoint_dir, exist_ok=True)
+        checkpoint_dir = self._settings.train.checkpoint_dir.absolute() / self._run_id
+        model_state = nnx.state(self._model)
+        self._checkpointer.save(checkpoint_dir / tag, model_state)
+        self._checkpointer.wait_until_finished()
+        print(f"Saved checkpoint: {self._run_id}/{tag}.")
 
 
 def run():
     import tyro
     settings = tyro.cli(Settings)
-    train_loop(settings)
+    Train(settings).run()
 
 
 if __name__ == '__main__':
